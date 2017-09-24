27d3aaa2fe308cf083640a1564ae9230ba4639fc
tomyspt <saputratomy325@gmail.com>
2017-09-24 13:26:03 +0700

    mengumpulkan tugas 10

---
+++ code4.php
@@ -0,0 +1,30 @@
+<?php
+ 	class smartphone {
+ 		public function __construct() {
+ 			echo "Constructor dari class smartphone <br />";
+ 		}
+ 		public function __destruct() {
+ 			echo "Constructor dari class smartphone_singlesim <br />";
+ 		}
+ 	}
+ 	class smartphone_dualsim extends smartphone {
+ 		public function __construct() {
+ 			echo "Constructor dari class smartphone_dualsim <br />";
+ 		}
+ 		public function __destruct() {
+ 			echo "Destructor dari class smartphone_dualsim <br />";
+ 		}
+ 	}
+ 	class smartphone_singlesim extends smartphone_dualsim {
+ 		public function __construct() {
+ 			echo "Destructor dari class smartphone_singlesim <br />";
+ 		}
+ 		public function __destruct() {
+ 			echo "Destructor dari class smartphone <br />";
+ 		}
+ 	}
+ 	$gadget_baru = new smartphone();
+ 	$gadget_baru = new smartphone_dualsim();
+ 	echo "Belajar OOP PHP <br />";
+ 	$gadget_baru = new smartphone_singlesim();
+ ?> 
